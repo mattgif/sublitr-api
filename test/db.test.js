d69a17deb6d4aec817a126d6c81a5a88368cc3f8
Matt Gifford <mattgif@gmail.com>
2018-03-26 10:19:42 -0400

    direct user creation through mongoose tested

---
+++ test/db.test.js
@@ -0,0 +1,52 @@
+const chai = require('chai');
+const mongoose = require('mongoose');
+
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+const {User} = require('../users/models');
+
+const expect = chai.expect;
+
+function tearDownDb() {
+    return mongoose.connection.dropDatabase();
+}
+
+describe('db connection', () => {
+    const email = 'user@example.com';
+    const password = 'passtest123';
+    const firstName = 'Testy';
+    const lastName = 'Testman';
+
+    before(function() {
+        return runServer(TEST_DATABASE_URL)
+    });
+
+    afterEach(function() {
+        return tearDownDb();
+    });
+
+    after(function() {
+        return closeServer()
+    });
+
+    it('should create a new user', () => {
+        return User.create({email,firstName,lastName,password}).then(user => {
+            expect(user.firstName).to.equal(firstName);
+            expect(user.lastName).to.equal(lastName);
+            expect(user.email).to.equal(email);
+        })
+    });
+
+    it('should have a serialize method that returns user with correct keys', () => {
+        return User.create({email,firstName,lastName,password}).then(user => {
+            return user.serialize()
+        })
+            .then(user => {
+                const fields = ['email', 'firstName', 'lastName', 'admin', 'editor'];
+                fields.forEach(field => {
+                    expect(field in user).to.be.true;
+                });
+                expect('password' in user).to.be.false;
+            });
+    })
+});
