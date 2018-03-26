bcca70d5310361416a0163924e40e3e06e671cb0
Matt Gifford <mattgif@gmail.com>
2018-03-26 10:53:03 -0400

    local strategy failing

--- auth/strategies.js
+++ auth/strategies.js
@@ -1,7 +1,12 @@
 'use strict';
-const {Strategy: LocalStrategy} = require('passport-local');
+const { Strategy: LocalStrategy } = require('passport-local');
 
-const {User} = require('../users/models');
+// Assigns the Strategy export to the name JwtStrategy using object destructuring
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Assigning_to_new_variable_names
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+
+const { User } = require('../users/models');
+const { JWT_SECRET } = require('../config');
 
 const localStrategy = new LocalStrategy((email, password, callback) => {
     let user;
@@ -9,29 +14,30 @@ const localStrategy = new LocalStrategy((email, password, callback) => {
         .then(_user => {
             user = _user;
             if (!user) {
+                // Return a rejected promise so we break out of the chain of .thens.
+                // Any errors like this will be handled in the catch block.
                 return Promise.reject({
                     reason: 'LoginError',
                     message: 'Incorrect email or password'
-                })
+                });
             }
-            return user.validatePassword(password)
+            return user.validatePassword(password);
         })
         .then(isValid => {
             if (!isValid) {
                 return Promise.reject({
                     reason: 'LoginError',
                     message: 'Incorrect email or password'
-                })
+                });
             }
             return callback(null, user);
         })
         .catch(err => {
             if (err.reason === 'LoginError') {
-                return callback(null, false, err)
+                return callback(null, false, err);
             }
             return callback(err, false);
-        })
+        });
 });
 
-
-module.exports = localStrategy;
+module.exports = {localStrategy};
